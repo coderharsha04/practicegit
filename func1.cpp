5afbe543a65689d65ddf080ac7018a0598051a3e
coderharsha04 <111386969+coderharsha04@users.noreply.github.com>
2023-10-07 17:22:48 +0530

    Update func1.cpp

--- func1.cpp
+++ func1.cpp
@@ -6,12 +6,12 @@ int main()
     int m = 20;
     for (int i = 0; i < m; i++)
     {
-        cout << i << endl;
+        cout << i * 2 << endl;
     }
 
-    for (int i = 0; i < m; i++)
+    for (int i = 1; i < m; i++)
     {
-        cout << i << endl;
+        cout << i * 4 << endl;
     }
 
     return 0;
