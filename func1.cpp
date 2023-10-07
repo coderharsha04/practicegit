0215c234e946e7cc23e9c90180662b3f69bf1b4e
coderharsha04 <111386969+coderharsha04@users.noreply.github.com>
2023-10-07 17:21:02 +0530

    files added (#1)

---
+++ func1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m = 20;
+    for (int i = 0; i < m; i++)
+    {
+        cout << i << endl;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 20;
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
