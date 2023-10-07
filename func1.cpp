3cf3d909545d7ecc8816036bdf98810d289bbbc9
coderharsha04 <111386969+coderharsha04@users.noreply.github.com>
2023-10-07 18:00:10 +0530

    User/h1/main function (#2)
    * files added
    
    * abs

--- func1.cpp
+++ func1.cpp
@@ -6,12 +6,12 @@ int main()
     int m = 20;
     for (int i = 0; i < m; i++)
     {
-        cout << i * 2 << endl;
+        cout << i * 8 << endl;
     }
 
     for (int i = 1; i < m; i++)
     {
-        cout << i * 4 << endl;
+        cout << i * 9 << endl;
     }
 
     return 0;
