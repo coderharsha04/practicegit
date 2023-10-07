11b9082acf9919a1112b9a90f4bb65fac105ebe8
coderharsha04 <harshkumar.bt19cse@pec.edu.in>
2023-10-07 17:23:32 +0530

    abs

--- func1.cpp
+++ func1.cpp
@@ -6,12 +6,12 @@ int main()
     int m = 20;
     for (int i = 0; i < m; i++)
     {
-        cout << i << endl;
+        cout << i * 8 << endl;
     }
 
     for (int i = 0; i < m; i++)
     {
-        cout << i << endl;
+        cout << i * 9 << endl;
     }
 
     return 0;
